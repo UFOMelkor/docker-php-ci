4fc371bbbb49a0ed94abec4c060d349c18200526
Oskar Bley <oskar.bley@atino.de>
2016-03-02 12:26:40 +0100

    Add flags to disable features

--- entrypoint_atino.sh
+++ entrypoint_atino.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-composer self-update
+if [[ -z "$WITHOUT_ZIP_EXTENSION" ]]
+then
+    docker-php-ext-enable zip
+fi
+
+if [[ -z "$WITHOUT_COMPOSER_SELF_UPDATE" ]]
+then
+    composer self-update
+fi
 
 /bin/bash /entrypoint.sh
 
